2fb53093f9542b668a3843b955f543cb57436e56
Czeglédi Levente <celevicte@gmail.com>
2025-10-27 15:48:54 +0100

    add deployment and barebones env setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { env } from '@/env'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
@@ -6,7 +7,12 @@ function App() {
   return (
     <div className="flex items-center justify-center h-screen flex-col">
       <h1 className="text-4xl font-bold text-center">UM Aréna Átrium</h1>
-      <p className="text-lg text-gray-500 text-center">Fejlesztés alatt...</p>
+      <p className="text-lg text-center">Fejlesztés alatt...</p>
+      <p>
+        Az app, amit nézel{' '}
+        <span className="italic text-muted-foreground">{env.VITE_APP_ENV}</span>{' '}
+        környezetben fut
+      </p>
     </div>
   )
 }
